f08fa7f2f0871c92f018e10eaee0a86c18ddee80
Joonas Sarajärvi <muepsj@gmail.com>
2011-07-19 22:56:27 +0300

    add more code to format stuff

--- feta.scm
+++ feta.scm
@@ -1,7 +1,11 @@
 #!/usr/bin/guile -s
 !#
 
+;; Line-oriented port reading
 (use-modules (ice-9 rdelim))
+
+;; Time/date library.
+(use-modules (srfi srfi-19))
 
 ;; Date conversion code
 (define full-date-format "~Y-~m-~dT~H:~M:~S")
@@ -13,26 +17,62 @@
   '("~H:~M:~S"
     "~H:~M"))
 
-(define fallbacky-string->date
+(define user-time-format
+  "~Y-~m-~dT ~H:~M")
+(define user-nonexistent-time-format
+  "(time ain't defined)")
+
+(define time->string
+  (lambda (time)
+    (if (time? time)
+        (date->string (time-utc->date time) user-time-format)
+        user-nonexistent-time-format)))
+
+;; Warn function that does nothing
+(define warn
+  (lambda (msg) #f))
+
+(define fallbacky-string->time
   (lambda (str formats)
     (if (null? formats)
+        ;; We ran out of formats.
         #f
+        ;; Try to parse with the first format in format list.
         (catch 'misc-error
+               ;; The actual attepmt to convert is here.
                (lambda ()
-                 (string->date str (car formats)))
+                 (date->time-utc (string->date str (car formats))))
+               ;; On failure, just keep trying with the rest of the
+               ;; format list.
                (lambda _
                  (fallbacky-string->date str (cdr formats)))))))
 
-(define permissive-string->date
+;; Just a convenience wrapper
+(define permissive-string->time
   (lambda (str)
-    (fallbacky-string->date str date-formats)))
+    (fallbacky-string->time str date-formats)))
 
+;; Also need a way to extract times from database format.
+;; This is easier because we need not support so many forms of input.
+(define db-string->time
+  (lambda (str)
+    (catch #t
+           (lambda ()
+             (make-time 'time-utc 0
+                        (string->number str)))
+           (lambda _ #f))))
 
 (define parse-db-line
   (lambda (line)
     (let* ((tokens (string-split line #\;))
-           (start-time (string->number (car tokens)))
-           (end-time (string->number (car (cdr tokens))))
+           (start-time (make-time
+                        'time-utc
+                        0
+                        (string->number (car tokens))))
+           (end-time (make-time
+                      'time-utc
+                      0
+                      (string->number (car (cdr tokens)))))
            (description (string-join (cddr tokens))))
 
       (acons 'start-time start-time
@@ -43,7 +83,13 @@
   (lambda (port f prevs)
     (let ((line (read-line port)))
       (if (string? line)
-          (cons (f line) (read-lines-with port f prevs))
+          (catch #t
+                 (lambda ()
+                   (cons
+                    (f line)
+                    (read-lines-with port f prevs)))
+                 (lambda _
+                   (read-lines-with port f prevs)))
           prevs))))
 
 (define read-lines
@@ -57,5 +103,30 @@
 (define db-location (string-append (getenv "HOME") "/.ttdb"))
 (define open-db (lambda () (open-file db-location "r")))
 
-(display (parse-db-port (open-db)))
-(newline)
+(define session->userline
+  (lambda (session)
+    (let* ((from (cdr (assoc 'start-time session)))
+           (to (cdr (assoc 'end-time session))))
+      (string-join
+       (list
+        "From "
+        (time->string from)
+        " to "
+        (time->string to)
+        " ("
+        (number->string
+         (time-second
+          (time-difference from
+                           (if (time? to)
+                               to
+                               (current-time 'time-utc)))))
+        ") seconds\n")
+       ""))))
+
+
+(define display-sessionlist
+  (lambda (sessions)
+    (for-each display (map session->userline sessions))))
+
+
+(display-sessionlist (parse-db-port (open-db)))
