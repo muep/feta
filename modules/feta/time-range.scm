7c0acb6dfc50c26b63f7cd233822753eccc2eb73
Joonas Sarajärvi <muep@iki.fi>
2011-09-04 17:40:41 +0300

    add time-range module

---
+++ modules/feta/time-range.scm
@@ -0,0 +1,69 @@
+;; time-range.scm
+;;
+;; Time range data type
+
+(define-module (feta time-range)
+  :export (;; Functions
+           make-time-range
+           time-range?
+           time-range-duration
+           time-range-end
+           time-range-start)
+  :use-module ((srfi srfi-19)
+               :select (make-time
+                        time?
+                        time<?
+                        time-difference
+                        time-utc)))
+
+(define tr-type
+  (make-record-type "time-range" '(start length)))
+
+(define tr-make
+  (record-constructor tr-type '(start length)))
+
+(define tr-start
+  (record-accessor tr-type 'start))
+(define tr-len
+  (record-accessor tr-type 'length))
+
+;; Convert a time given as an unix time stamp number into
+;; the srfi-19 time record.
+(define timize
+  (lambda (ton)
+    (cond ((time? ton) ton)
+          ((number? ton) (make-time 'time-utc 0 ton))
+          (#t #f))))
+
+(define earliest
+  (lambda (times)
+    (if (equal? (length times) 1)
+        (car times)
+        (let ((t1 (car times))
+              (t2 (cadr times))
+              (rest (cddr times)))
+          (earliest (cons
+                     (if (time<? t1 t2) t1 t2)
+                     rest))))))
+
+(define normalise-start-end
+  (lambda (start end)
+    (let ((start-time (cond ((time? start) start)
+                            ((number? start)
+                             )))))))
+
+(define make-time-range
+  (lambda (start end)
+    (let ((times (sort (list (timize start) (timize end)) time<?)))
+      (tr-make (car times) (time-difference (cadr times) (car times))))))
+
+(define time-range?
+  (record-predicate tr-type))
+
+(define time-range-duration
+  tr-len)
+(define time-range-end
+  (lambda (tr) (add-duration (tr-start tr)
+                             (tr-len tr))))
+(define time-range-start
+  tr-start)
