6b772c083eec872c5cb46b9390b15ed98ae74648
Joonas Sarajärvi <muep@iki.fi>
2011-10-16 13:27:42 +0300

    add make-session-from to (feta session)

--- modules/feta/session.scm
+++ modules/feta/session.scm
@@ -4,11 +4,13 @@
 
 (define-module (feta session)
   :export (make-session
+           make-session-from
            session?
            session-description
            session-end
            session-start
            session-time-range)
+  :use-module (ice-9 optargs)
   :use-module (feta nih)
   :use-module (feta time-range))
 
@@ -16,6 +18,17 @@
 ;; implementation detail of alistness.
 (define (make-session descr tr)
   (acons 'description descr tr))
+
+(define* (make-session-from session
+                            #:key end description)
+  (append
+   (if end
+       (make-time-range (session-start session)
+                        end)
+       '())
+   (if description
+       (list (cons 'description 'description)) '())
+   session))
 
 (define (session? s)
   (if (and (time-range? s)
