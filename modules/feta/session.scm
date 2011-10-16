9accf2aaf8db9d48be1507f47614bb542ed39ab1
Joonas Sarajärvi <muep@iki.fi>
2011-10-16 12:44:02 +0300

    take short-cuts in session

--- modules/feta/session.scm
+++ modules/feta/session.scm
@@ -6,22 +6,24 @@
   :export (make-session
            session?
            session-description
+           session-end
+           session-start
            session-time-range)
+  :use-module (feta nih)
   :use-module (feta time-range))
 
-(define s-type
-  (make-record-type "session" '(description time-range)))
+;; Here we quite rudely depend on our time-range
+;; implementation detail of alistness.
+(define (make-session descr tr)
+  (acons 'description descr tr))
 
-(define s-make
-  (record-constructor s-type '(description time-range)))
+(define (session? s)
+  (if (and (time-range? s)
+           (assoc 'description s)) #t #f))
 
-(define s-descr
-  (record-accessor s-type 'description))
+(define (session-description s)
+  (aget 'description s))
 
-(define s-tr
-  (record-accessor s-type 'time-range))
-
-(define make-session s-make)
-(define session? (record-predicate s-type))
-(define session-description s-descr)
-(define session-time-range s-tr)
+(define session-start time-range-start)
+(define session-end time-range-end)
+(define (session-time-range s-tr) s-tr)
