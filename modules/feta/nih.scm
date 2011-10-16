44c722fcd554bc8ff77fd3c122a43581d205f253
Joonas Sarajärvi <muep@iki.fi>
2011-10-16 13:24:25 +0300

    add a get-kval function to (feta nih)

--- modules/feta/nih.scm
+++ modules/feta/nih.scm
@@ -6,6 +6,7 @@
 (define-module (feta nih)
   :export (aget
            all
+           get-kval
            uniquify))
 
 ;; Get from association list, or #f
@@ -20,6 +21,16 @@
     (cond ((null? l) #t)
           ((not (ok? (car l))) #f)
           (#t (all ok? (cdr l))))))
+
+;; Fetches the first item from l whose previous item is eq
+;; with k. If no item is found, returns #f.
+(define (get-kval k l)
+  (let ((kl (member k l)))
+    (if (or (not (list? kl))
+            (null? kl)
+            (not (list? (cdr kl)))
+            (null? (cdr kl)))
+        #f (cadr kl))))
 
 (define remove-adjacents
   (lambda (list match? prev)
